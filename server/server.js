0549e32a8a851ed7c968dfaf5c0f749c6fee0876
h1-wmb <75176905+h1-wmb@users.noreply.github.com>
2021-01-22 10:33:55 +0000

    Fixed the bypass of name trimming causing injection of unwanted code leading to RCE.

--- server/server.js
+++ server/server.js
@@ -24,17 +24,13 @@ var socketsh = crypto.createHash('sha512').update(socket.id).digest('hex');
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and room name are required.');
-    }
-
+    } else {
     if (params.name.length > 16) {
       return callback('Name cannot be more than 16 characters.');
-    }
-
+    } else {
     var roomsh = crypto.createHash('sha512').update(params.room).digest('hex');
     socket.join(roomsh);
     users.removeUser(socketsh);
-    console.log(socketsh);
-    console.log(socketsh);
     users.addUser(socketsh, params.name, roomsh);
     io.to(roomsh).emit('updateUserList', users.getUserList(roomsh));
     socket.emit('newMessage', generateMessage('Server', 'Welcome to Whittr!'));
@@ -42,6 +38,7 @@ var socketsh = crypto.createHash('sha512').update(socket.id).digest('hex');
     const randomOpening = opening[Math.floor(Math.random() * opening.length)];
     socket.broadcast.to(roomsh).emit('newMessage', generateMessage('Server - Welcome', `"${params.name}" ${randomOpening}`));
     callback();
+    };
   });
 
   socket.on('createMessage', (message, callback) => {
