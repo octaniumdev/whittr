795fe47f062c4acf25b4877bfa6b76310a3d0977
Cob:web Development <75907419+cobwebdev@users.noreply.github.com>
2021-02-13 12:14:17 +0000

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -44,8 +44,7 @@ socket.on('updateUserList', function (users) {
 });
 
 socket.on('updateROOMID', function (id) {
-  var ol = jQuery('<ol></ol>');
-  ol.append(jQuery('<h3></h3>').text(id));
+  ol.append(jQuery('<li></li>').text(id));
   jQuery('#roomid').html(ol);
 });
 
