4bf5b1f77102b4c3587fb10f472373c337bb7713
Locness <locness3@e.email>
2021-04-13 14:40:49 +0200

    Beautify index.js + use express.static

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var crypto = require("crypto"),
     fs = require("fs"),
     sio = require("socket.io"),
     path = require('path'),
-http = require("http");
+    http = require("http");
 
 var moment = require('moment');
 var app = express();
@@ -26,41 +26,45 @@ var publicPath = path.join(__dirname, '/public');
 
 
 class UserID {
-constructor () {
-    this.users = [];
-};
-
-addUser (id, name, room) {
-    var user = {id, name, room};
-    this.users.push(user);
-    return user;
-};
-
-removeUser (id) {
-    var user = this.getUser(id);
-    if (user) {
-    this.users = this.users.filter((user) => user.id !== id);
+    constructor() {
+        this.users = [];
     };
-    return user;
-};
 
-getUser (id) {
-    return this.users.filter((user) => user.id === id)[0]
-};
+    addUser(id, name, room) {
+        var user = {
+            id,
+            name,
+            room
+        };
+        this.users.push(user);
+        return user;
+    };
 
-getUserList (room) {
-    var users = this.users.filter((user) => user.room === room);
-    var namesArray = users.map((user) => user.name);
-    return namesArray;
-};
+    removeUser(id) {
+        var user = this.getUser(id);
+        if (user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        };
+        return user;
+    };
+
+    getUser(id) {
+        return this.users.filter((user) => user.id === id)[0]
+    };
+
+    getUserList(room) {
+        var users = this.users.filter((user) => user.room === room);
+        var namesArray = users.map((user) => user.name);
+        return namesArray;
+    };
 };
 var users = new UserID();
 
 var generateMessage = (from, text) => {
-return {
-    from,
-    text,
-    createdAt: moment().valueOf()
+    return {
+        from,
+        text,
+        createdAt: moment().valueOf()
     };
 };
 
@@ -81,136 +85,76 @@ var encrypt = ((val) => {
     encrypted += cipher.final("base64");
     return encrypted;
 });
-  
+
 var decrypt = ((encrypted) => {
     let decipher = crypto.createDecipheriv("aes-256-cbc", aeshash, IV);
     let decrypted = decipher.update(encrypted, "base64", "utf8");
     return (decrypted + decipher.final("utf8"));
 });
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath, {extensions: ['html']}));
 
-app.get('/', function (req, res) {
-    res.sendFile(publicPath + "/index.html");
-});
-
-app.get('/index', function (req, res) {
-    res.sendFile(publicPath + "/index.html");
-});
-
-app.get('/chat', function (req, res) {
-    res.sendFile(publicPath + "/chat.html");
-});
-
-app.get('/create', function (req, res) {
-    res.sendFile(publicPath + "/create.html");
-});
-
-app.get('/rules', function (req, res) {
-    res.sendFile(publicPath + "/rules.html");
-});
-
-app.get('/join', function (req, res) {
-    res.sendFile(publicPath + "/join.html");
-});
-
-app.get('/tos', function (req, res) {
-    res.sendFile(publicPath + "/tos.html");
-});
-
-app.get('/js/chat.js', function (req, res) {
-    res.sendFile(publicPath + "/js/chat.js");
-});
-
-app.get('/js/libs/deparam.js', function (req, res) {
-    res.sendFile(publicPath + "/js/libs/deparam.js");
-});
-
-app.get('/js/libs/jquery-3.1.0.min.js', function (req, res) {
-    res.sendFile(publicPath + "/js/libs/jquery-3.1.0.min.js");
-});
-
-app.get('/js/libs/mustache.js', function (req, res) {
-    res.sendFile(publicPath + "/js/libs/mustache.js");
-});
-
-app.get('/css/style.css', function (req, res) {
-    res.sendFile(publicPath + "/css/style.css");
-});
-
-app.get('/css/chat.css', function (req, res) {
-    res.sendFile(publicPath + "/css/chat.css");
-});
-
-app.get('/assets/background.png', function (req, res) {
-    res.sendFile(publicPath + "/assets/background.png");
-});
-
-app.get('/assets/icon.png', function (req, res) {
-    res.sendFile(publicPath + "/assets/icon.png");
-});
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.sendFile(publicPath + "/404.html");
 });
 
 io.on("connection", (socket) => {
-var id = crypto.createHmac("sha512", secret).update(socket.id).digest("hex");
-socket.on("join", (req, res) => {
-    if (!isRealString(req.name) || !isRealString(req.room)) {
-    return res("Name and room pin are required.");
-    } else {
-    if (req.name.length > 16) {
-    return res("Name cannot be more than 16 characters.");
-    } else {
-    if (req.room.length > 8) {
-    return res("Room pin cant be more than 8 digits.");
-    } else {
-    var room = crypto.createHmac("sha512", secret).update(req.room).digest("hex");
-    socket.join(room);
-    users.removeUser(id);
-    users.addUser(id, req.name, room);
-    io.to(room).emit("updateUserList", users.getUserList(room));
-    socket.emit("newMessage", generateMessage("Server", `Welcome to Whittr, your room code is ${req.room}.`));
-    socket.emit("updateROOMID", `${req.room}`);
-    var opening = ["has entered the arena!", "is here to fight!", "is here to chew gum and fight crime!", "has stolen your pizza!", "just joined the chat, glhf!", "just joined, everyone pretend you're busy!", "joined your party.", "We have been expecting you ( ͡° ͜ʖ ͡°)", "has brought pizza!", "please leave your weapons by the door.", "has appeared.", "just slid into the chat ( ͡° ͜ʖ ͡°)", "has just landed.", "needs to be nerfed", "is here to slide into your DMs"];
-    var randomOpening = opening[Math.floor(Math.random() * opening.length)];
-    socket.broadcast.to(room).emit("newMessage", generateMessage("Server - Welcome", `"${req.name}" ${randomOpening}`));
-    res();
-    };
-    };
-    };
-    return res("An error has occured.");
-});
+    var id = crypto.createHmac("sha512", secret).update(socket.id).digest("hex");
+    socket.on("join", (req, res) => {
+        if (!isRealString(req.name) || !isRealString(req.room)) {
+            return res("Name and room pin are required.");
+        } else {
+            if (req.name.length > 16) {
+                return res("Name cannot be more than 16 characters.");
+            } else {
+                if (req.room.length > 8) {
+                    return res("Room pin cant be more than 8 digits.");
+                } else {
+                    var room = crypto.createHmac("sha512", secret).update(req.room).digest("hex");
+                    socket.join(room);
+                    users.removeUser(id);
+                    users.addUser(id, req.name, room);
+                    io.to(room).emit("updateUserList", users.getUserList(room));
+                    socket.emit("newMessage", generateMessage("Server", `Welcome to Whittr, your room code is ${req.room}.`));
+                    socket.emit("updateROOMID", `${req.room}`);
+                    var opening = ["has entered the arena!", "is here to fight!", "is here to chew gum and fight crime!", "has stolen your pizza!", "just joined the chat, glhf!", "just joined, everyone pretend you're busy!", "joined your party.", "We have been expecting you ( ͡° ͜ʖ ͡°)", "has brought pizza!", "please leave your weapons by the door.", "has appeared.", "just slid into the chat ( ͡° ͜ʖ ͡°)", "has just landed.", "needs to be nerfed", "is here to slide into your DMs"];
+                    var randomOpening = opening[Math.floor(Math.random() * opening.length)];
+                    socket.broadcast.to(room).emit("newMessage", generateMessage("Server - Welcome", `"${req.name}" ${randomOpening}`));
+                    res();
+                };
+            };
+        };
+        return res("An error has occured.");
+    });
 
-socket.on("createMessage", (request, res) => {
-    if(request.text.length >= 200) {
-        socket.emit("newMessage", generateMessage("Server", "Your message cannot be more then 200 characters."));
-    } else {
-    var user = users.getUser(id);
-    if(user && isRealString(request.text)) {
-    const req = dehashenc(`${request.text}`, `${aeshash}`);
-    var reqraw = decrypt(req);
-    io.to(user.room).emit("newMessage", generateMessage(`${user.name}`, reqraw));
-    };
-    };
-    res();
-});
+    socket.on("createMessage", (request, res) => {
+        if (request.text.length >= 200) {
+            socket.emit("newMessage", generateMessage("Server", "Your message cannot be more then 200 characters."));
+        } else {
+            var user = users.getUser(id);
+            if (user && isRealString(request.text)) {
+                const req = dehashenc(`${request.text}`, `${aeshash}`);
+                var reqraw = decrypt(req);
+                io.to(user.room).emit("newMessage", generateMessage(`${user.name}`, reqraw));
+            };
+        };
+        res();
+    });
 
 
-socket.on("disconnect", () => {
-    var user = users.removeUser(id);
-    if (user) {
-    io.to(user.room).emit("updateUserList", users.getUserList(user.room));
-    const leave = ["got grumpy and left.", "was done with this chat.", "did unspeakable things and had to leave...", "regrets not saying goodbye.", "has to apologise for something!", "stole your pizza and got away with it.", "is eating your pizza", "has stolen the princess!", "is a titan!", "is not ready for this groupchat just yet.", "is too full to eat anymore cake."];
-    const randomLeave = leave[Math.floor(Math.random() * leave.length)];
-    io.to(user.room).emit("newMessage", generateMessage("Server - Leave", `"${user.name}" ${randomLeave}`));
-    }
+    socket.on("disconnect", () => {
+        var user = users.removeUser(id);
+        if (user) {
+            io.to(user.room).emit("updateUserList", users.getUserList(user.room));
+            const leave = ["got grumpy and left.", "was done with this chat.", "did unspeakable things and had to leave...", "regrets not saying goodbye.", "has to apologise for something!", "stole your pizza and got away with it.", "is eating your pizza", "has stolen the princess!", "is a titan!", "is not ready for this groupchat just yet.", "is too full to eat anymore cake."];
+            const randomLeave = leave[Math.floor(Math.random() * leave.length)];
+            io.to(user.room).emit("newMessage", generateMessage("Server - Leave", `"${user.name}" ${randomLeave}`));
+        }
     });
 });
 
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+    console.log(`Server is up on ${port}`);
 });
